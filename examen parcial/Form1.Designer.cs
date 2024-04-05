f6054371c9af17ebcee96cf73bcf48fb3b42a3da
Dominick291 <dpalpirez@gmail.com>
2024-04-04 21:41:56 -0600

    punto 2

--- examen parcial/Form1.Designer.cs
+++ examen parcial/Form1.Designer.cs
@@ -30,58 +30,141 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.comboBoxAlumnos = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(63, 57);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(184, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Seleccione al alumno:";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(370, 91);
+            this.dataGridView1.Location = new System.Drawing.Point(368, 45);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(431, 148);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Location = new System.Drawing.Point(37, 353);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(148, 67);
+            this.buttonGuardar.TabIndex = 2;
+            this.buttonGuardar.Text = "GUARDAR";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            this.buttonGuardar.Click += new System.EventHandler(this.buttonGuardar_Click);
+            // 
+            // comboBoxAlumnos
+            // 
+            this.comboBoxAlumnos.FormattingEnabled = true;
+            this.comboBoxAlumnos.Location = new System.Drawing.Point(38, 86);
+            this.comboBoxAlumnos.Name = "comboBoxAlumnos";
+            this.comboBoxAlumnos.Size = new System.Drawing.Size(227, 21);
+            this.comboBoxAlumnos.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 292);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(38, 160);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(271, 89);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Talleres disponibles:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(286, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ingrese el codigo del taller a inscribirse:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(57, 246);
+            this.button1.Location = new System.Drawing.Point(466, 409);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 67);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 54);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Ordenar alfabeticamente";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // comboBox1
+            // dataGridView3
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(66, 117);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 3;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(368, 263);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(431, 123);
+            this.dataGridView3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(365, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Datos";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(897, 490);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView3);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBoxAlumnos);
+            this.Controls.Add(this.buttonGuardar);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,8 +174,15 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonGuardar;
+        private System.Windows.Forms.ComboBox comboBoxAlumnos;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- examen parcial/Form1.cs
+++ examen parcial/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace examen_parcial
 {
@@ -21,6 +22,7 @@ namespace examen_parcial
         List<Alumnos> alumnos = new List<Alumnos>();
         List<Talleres> talleres = new List<Talleres>();
         List<Inscripciones> inscripciones = new List<Inscripciones>();
+        List<Reporte> reportes = new List<Reporte>();
 
 
         public void CargarAlumnos()
@@ -28,7 +30,7 @@ namespace examen_parcial
             String fileName = "Alumnos.txt";
 
 
-            FileStream stream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Read);
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
             StreamReader reader = new StreamReader(stream);
             while (reader.Peek() > -1)
             {
@@ -37,9 +39,14 @@ namespace examen_parcial
                 alumnoss.Nombre = reader.ReadLine();
                 alumnoss.Direccion = reader.ReadLine();
 
-
+                alumnos.Add(alumnoss);
             }
             reader.Close();
+
+            comboBoxAlumnos.DisplayMember = "Nombre";
+            comboBoxAlumnos.ValueMember = "Dpi";
+            comboBoxAlumnos.DataSource = alumnos;
+            comboBoxAlumnos.Refresh();
         }
 
         public void cargarTalleres()
@@ -47,7 +54,7 @@ namespace examen_parcial
             String fileName = "Talleres.txt";
 
 
-            FileStream stream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Read);
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
             StreamReader reader = new StreamReader(stream);
             while (reader.Peek() > -1)
             {
@@ -56,11 +63,16 @@ namespace examen_parcial
                 talleress.NombreTaller = reader.ReadLine();
                 talleress.Costo = Convert.ToDecimal(reader.ReadLine());
 
+                talleres.Add(talleress);
             }
             reader.Close();
+
+            dataGridView2.DataSource = null;
+            dataGridView2.DataSource = talleres;
+            dataGridView2.Refresh();
         }
 
-
+        
 
 
         private void Form1_Load(object sender, EventArgs e)
@@ -69,5 +81,36 @@ namespace examen_parcial
             cargarTalleres();
         }
 
+        private void buttonGuardar_Click(object sender, EventArgs e)
+        {
+
+            Inscripciones inscripcioness = new Inscripciones(); 
+            
+            inscripcioness.DpiAlumnos = Convert.ToInt32(comboBoxAlumnos.SelectedValue);
+            inscripcioness.CodTaller = Convert.ToInt32(textBox1.Text);
+            inscripcioness.Fecha = DateTime.Now;
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = inscripciones;
+            dataGridView1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Refresh();
+            dataGridView1.DataSource = "";
+
+            Reporte reporte = reportes.OrderByDescending(a => a.Nombre).First();
+            reportes.Clear();
+            reportes.Add(reporte);
+
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = reportes;
+            dataGridView1.Refresh();
+        }
     }
 }
